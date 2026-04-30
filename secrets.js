4b1a29cff77288a64e652a42500d1086567b3844
Steve Winton <stevewinton@gmail.com>
2026-04-30 16:07:54 -0500

    🤫

---
+++ secrets.js
@@ -0,0 +1,19 @@
+// Intentionally fake credentials for secret-scanning demos only
+
+const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+const githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwxyz";
+
+const slackToken = "xoxb-123456789012-123456789012-abcdefghijklmnopqrstuvwxyz123456";
+
+const stripeApiKey = "sk_live_1234567890abcdefghijklmnopqrstuvwxyz";
+
+const privateKey = `
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASC...
+FAKEKEYMATERIALFORDEMOONLY123456
+-----END PRIVATE KEY-----
+`;
+
+console.log("Demo secrets loaded");
